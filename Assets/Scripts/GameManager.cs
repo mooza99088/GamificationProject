d33f294aa55e4e7844d5858658bd2d8771cc79d3
mooza99088 <christopherpobrien2015@gmail.com>
2022-12-05 10:23:33 +0800

    Teleport changes

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -28,7 +28,7 @@ public class GameManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Cancel") == true)
+        if(Input.GetKeyDown(KeyCode.Tab) == true)
         {
             TogglePauseGame();
         }     
